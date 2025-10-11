c1f3a175620bd2eeec0c4948ab91bbc111231a9d
HarukaRepo <135007919+Fandyfr@users.noreply.github.com>
2025-10-11 19:21:24 +0800

    Update gitignore + Vercel Tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import "../styles/globals.css";
 import NextNProgress from "nextjs-progressbar";
 import { useRouter } from "next/router";
 import { motion as m } from "framer-motion";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -10,6 +12,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <m.div>
+        <SpeedInsights />
+        <Analytics />
         <NextNProgress color="#51FF76FF" height={3} />
         <Component {...pageProps} />
       </m.div>
